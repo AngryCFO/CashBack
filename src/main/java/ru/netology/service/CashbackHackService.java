7fcf4733b512b81f81d41937e9ee80228c5efec3
AngryCFO <147319756+AngryCFO@users.noreply.github.com>
2024-10-06 23:50:46 +0300

    Update CashbackHackService.java

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,6 +4,9 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
+        if (amount == boundary) {
+            return 0;
+        }
         return boundary - amount % boundary;
     }
 }
