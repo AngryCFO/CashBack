ae15fed8fb2b230a067e55979b08826948e5ddcd
AngryCFO <147319756+AngryCFO@users.noreply.github.com>
2024-10-06 23:07:45 +0300

    Update CashbackHackService.java

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -1,34 +1,9 @@
 package ru.netology.service;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+public class CashbackHackService {
+    private final int boundary = 1000;
 
-public class CashbackHackServiceTest {
-
-    @Test
-    public void shouldCalculateMissingQty() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 999;
-        int expected = 1;
-        int actual = service.remain(amount);
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    public void amountEqualBoundary() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-        int expected = 0;
-        int actual = service.remain(amount);
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    public void amountMoreBoundary() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1001;
-        int expected = 999;
-        int actual = service.remain(amount);
-        assertEquals(actual, expected);
+    public int remain(int amount) {
+        return boundary - amount % boundary;
     }
 }
