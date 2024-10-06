d9ad6bd7a462a2dd663caf838816850b69842dd1
AngryCFO <a79955700981@yandex.ru>
2024-10-07 00:34:15 +0300

    Changed 3 files

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,9 +1,35 @@
 package ru.netology.service;
 
-public class CashbackHackService {
-    private final int boundary = 1000;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.Assume;
 
-    public int remain(int amount) {
-        return boundary - amount % boundary;
+public class CashbackHackServiceTest {
+
+    @Test
+    void shouldCalculateMissingQty() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void amountEqualBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void amountMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int expected = 999;
+        int actual = service.remain(amount);
+        Assertions.assertEquals(expected, actual);
     }
 }
